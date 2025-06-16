8c298fbe93b034571e7af7124f4bab48f3cb3790
disa12311 <hadiepml@gmail.com>
2025-06-16 13:54:50 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
-// Trong file preload.js hoặc main.js
-const { dialog } = require('electron');
+// Trong file main.js
+const { ipcMain } = require('electron');
+const { execFile } = require('child_process');
 
-// Hàm để gọi từ React UI
-async function handleFileOpen() {
-  const { canceled, filePaths } = await dialog.showOpenDialog({
-    properties: ['openFile'],
-    filters: [
-      { name: 'Applications', extensions: ['exe', 'lnk'] }
-    ]
-  });
-  if (!canceled) {
-    return filePaths[0]; // Trả về đường dẫn file đã chọn
+ipcMain.on('launch-apps', (event, appPaths) => {
+  if (appPaths && appPaths.length > 0) {
+    console.log('Đang khởi chạy các ứng dụng:', appPaths);
+    appPaths.forEach(path => {
+      // execFile an toàn hơn exec khi đường dẫn có thể chứa dấu cách
+      execFile(path, (error) => {
+        if (error) {
+          console.error(`Lỗi khi chạy ${path}:`, error);
+          // Có thể gửi thông báo lỗi về lại cho UI
+        }
+      });
+    });
   }
-}
+});
